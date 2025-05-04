c9fcd4089dbb6f42d3ce2f97839e1e8eb671b7de
edgedpub <63003391+edgedpub@users.noreply.github.com>
2025-05-04 19:22:10 +0100

    Add files via upload

---
+++ 44/actions.js
@@ -0,0 +1,207 @@
+pubcoder.projectID = pubcoder.projectID || "2F3D23559A574BC6B006B65195797F50";
+pubcoder.project.id = pubcoder.project.id || "2F3D23559A574BC6B006B65195797F50";
+pubcoder.project.title = pubcoder.project.title || "The Harvest Fable";
+pubcoder.page.id = pubcoder.page.id || 418;
+pubcoder.page.title = pubcoder.page.title || "44";
+pubcoder.page.number = pubcoder.page.number || 44;
+pubcoder.page.alias = pubcoder.page.alias || "";
+
+var ua = navigator.userAgent.toLowerCase();
+var touchDownEvent;
+var touchUpEvent;
+var isMobile;
+var aigX = 0, aigY = 0;
+var askAudioPermission = false;
+
+/*
+ * 
+ * Init Action Lists
+ *
+ * 
+ */
+
+/*
+ * 
+ * Init SCCounter
+ *
+ * 
+ */
+ 
+ $(window).load(function(){
+	window.eventObj = {};
+	/*
+	 * 
+	 * Init SCAnimation
+	 * 
+	 * 
+	 */
+	
+	/*
+	 *
+	 *   Init Shake
+	 *
+	 */
+	window.addEventListener('shake', function () {
+		
+	}, false);
+	
+	/*
+	 *
+	 *   Init Masked Images
+	 *
+	 */
+	 
+ 	/*
+	 * 
+	 * Init SCPhotogallery
+	 * 
+	 * 
+	 */
+	
+ 	/*
+	 * 
+	 * Init SCQuizMulti
+	 * 
+	 * 
+	 */
+	
+ 	/*
+	 * 
+	 * Init SCDrawer
+	 * 
+	 * 
+	 */
+	
+    
+ 	/*
+	 * 
+	 * Init SCWPanZoom
+	 * 
+	 * 
+	 */
+	
+    
+ 	/*
+	 * 
+	 * Init SCWMemoryGame
+	 * 
+	 * 
+	 */
+	
+	/*
+	 * 
+	 * Init SCCrosswords
+	 * 
+	 * 
+	 */
+	
+	/*
+	 * 
+	 * Init SCFillInTheGaps
+	 * 
+	 * 
+	 */
+	
+	/*
+	 * 
+	 * Init SCTrueFalse
+	 * 
+	 * 
+	 */
+	
+	
+	/*
+	 * 
+	 * Init SCMatchWordWithPicture
+	 * 
+	 * 
+	 */
+	
+    
+    
+	if(! navigator.userAgent.match(/PubCoderHelper/i)) {
+		/*
+		 *
+	 	 *   Action Groups
+	 	 *
+	 	 */
+		
+		
+		/*
+		 *
+	 	 *  Events
+	 	 *
+	 	 */
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		/*
+		 *
+	 	 *  Page is ready to be played
+	 	 *
+	 	 */
+		if (askAudioPermission) {
+	// $("#SCAudioPermissionAllowButton").click(function () {
+	// 	$( "audio" ).each(function( index ) {
+	// 		this.play();
+	// 		this.pause();
+	// 	});
+	// 	$(window).trigger(PubCoder.Events.PagePlay);
+	// });
+	// $("#SCAudioPermissionCancelButton").click(function () {
+	// 	$(window).trigger(PubCoder.Events.PagePlay);
+	// });
+	// var remodalInst = $('[data-remodal-id=SCAudioPermissionAlert]').remodal().open();
+} else {
+	setTimeout(function() {
+		$(window).trigger(PubCoder.Events.PagePlay);
+	}, 100);
+}
+	 }
+});
+var orientationDevice = getOrientation(PubCoder.isAndroidReader);
+$(window).on(PubCoder.Events.PagePlay, function() {
+	if (pubcoder.isInteractionObserverSupported) {
+		var ob = new IntersectionObserver(function(entries) {
+			$(entries).each(function (index, entry) {
+				if (entry.isIntersecting) {
+					$(entry.target).trigger(pubcoder.events.appear);
+				} else {
+					$(entry.target).trigger(pubcoder.events.disappear);
+				}
+			});
+		}, {
+			root: null,
+			rootMargin: "0px",
+			threshold: 0
+		});
+		$(".SCView").each(function (i, el) {
+			ob.observe(el);
+		});	
+	}
+	
+$("#obj441").trigger('SCEventShow');
+$("#obj838").trigger('SCEventShow');
+	$(window).trigger(PubCoder.Events.PageReady);
+	
+});
